954b5b430198973e447ff88a0356e6b41adea85c
davidpodhola <david.podhola@naseukoly.cz>
2015-09-23 19:05:23 +0200

    test example

--- tests/fsharp-angular.UITests/Program.fs
+++ tests/fsharp-angular.UITests/Program.fs
@@ -1,7 +1,39 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿//these are similar to C# using statements
+open canopy
+open runner
+open System
 
-[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+//start an instance of the firefox browser
+start firefox
+
+//this is how you define a test
+"taking canopy for a spin" &&& fun _ ->
+    //this is an F# function body, it's whitespace enforced
+
+    //go to url
+    url "http://lefthandedgoat.github.io/canopy/testpages/"
+
+    //assert that the element with an id of 'welcome' has
+    //the text 'Welcome'
+    "#welcome" == "Welcome"
+
+    //assert that the element with an id of 'firstName' has the value 'John'
+    "#firstName" == "John"
+
+    //change the value of element with
+    //an id of 'firstName' to 'Something Else'
+    "#firstName" << "Something Else"
+
+    //verify another element's value, click a button,
+    //verify the element is updated
+    "#button_clicked" == "button not clicked"
+    click "#button"
+    "#button_clicked" == "button clicked"
+
+//run all tests
+run()
+
+printfn "press [enter] to exit"
+System.Console.ReadLine() |> ignore
+
+quit()
