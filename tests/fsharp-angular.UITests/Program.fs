947917d4e90b3707eb0725dce2cab5b3fd5059cb
davidpodhola <david.podhola@naseukoly.cz>
2015-09-23 19:46:22 +0200

    real test that simple angular controller is working

--- tests/fsharp-angular.UITests/Program.fs
+++ tests/fsharp-angular.UITests/Program.fs
@@ -1,36 +1,22 @@
-﻿//these are similar to C# using statements
-open canopy
+﻿open canopy
 open runner
 open System
 
-//start an instance of the firefox browser
 start firefox
 
-//this is how you define a test
-"taking canopy for a spin" &&& fun _ ->
-    //this is an F# function body, it's whitespace enforced
+"simple angular controller is working" &&& fun _ ->
 
     //go to url
-    url "http://lefthandedgoat.github.io/canopy/testpages/"
+    url "http://localhost:5523/"
 
-    //assert that the element with an id of 'welcome' has
-    //the text 'Welcome'
-    "#welcome" == "Welcome"
+    "#food" == "pizza"
 
-    //assert that the element with an id of 'firstName' has the value 'John'
-    "#firstName" == "John"
+    "#sentence" == "Sriracha sauce is great with pizza!"
 
-    //change the value of element with
-    //an id of 'firstName' to 'Something Else'
-    "#firstName" << "Something Else"
+    "#food" << "eggs"
 
-    //verify another element's value, click a button,
-    //verify the element is updated
-    "#button_clicked" == "button not clicked"
-    click "#button"
-    "#button_clicked" == "button clicked"
+    "#sentence" == "Sriracha sauce is great with eggs!"
 
-//run all tests
 run()
 
 quit()
