25a0c4c1d96c80239b067be1915fc2276374d49f
Julie Koubová <juliekoubova@icloud.com>
2015-09-25 15:11:14 +0200

    ui tests

--- tests/fsharp-angular.UITests/Program.fs
+++ tests/fsharp-angular.UITests/Program.fs
@@ -2,6 +2,8 @@
 open runner
 open System
 
+let testUrl  = "http://localhost:5523/"
+let waitSecs = 3
 start firefox
 
 "simple angular controller is working" &&& fun _ ->
@@ -16,6 +18,28 @@ start firefox
     "#food" << "eggs"
 
     "#sentence" == "Sriracha sauce is great with eggs!"
+
+
+"todo controller calculates remaining tasks" &&& fun _ ->
+    url testUrl
+    sleep waitSecs
+    ".todo-count" == "2 of 2 remaining"
+
+"todo controller loads two default tasks" &&& fun _ ->
+    url testUrl
+    sleep waitSecs
+    count ".todo-item" 2
+
+"todo controller adds a new task" &&& fun _ ->
+    url testUrl
+    sleep waitSecs
+    ".todo-new-title" << "test using Canopy"
+    click "input[type=submit]"
+    sleep waitSecs
+
+    ".todo-count" == "3 of 3 remaining"
+    count ".todo-item" 3
+    ".todo-item:nth-of-type(3)" == "test using Canopy"
 
 run()
 
